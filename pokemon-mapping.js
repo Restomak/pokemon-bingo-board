f9504af20a3ab220e5345924ed7d4433e1149a73
Restomak <38076002+Restomak@users.noreply.github.com>
2022-04-16 22:17:50 -0400

    remove delay, page doesn't like it

--- pokemon-mapping.js
+++ pokemon-mapping.js
@@ -193,7 +193,6 @@ function randomizeBoard(reroll = false) {
       mySeededRng = new Math.seedrandom('' + seed); // this is inconsistent if you pass a number instead of a string
       pokemonOnTheBoard = [];
       giveUpTimer++;
-      delay(1000);
     }
   } while (!exitLoop)
 

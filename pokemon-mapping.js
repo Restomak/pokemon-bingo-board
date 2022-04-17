71d0802a14bf05a5532d7fa53136dbc9d541f973
Restomak <38076002+Restomak@users.noreply.github.com>
2022-04-16 20:01:08 -0400

    oops

--- pokemon-mapping.js
+++ pokemon-mapping.js
@@ -27,7 +27,7 @@ let gen = genSelector.value;
 function randomizeBoard() {
   gen = genSelector.value;
 
-  //mySeededRng = new Math.seedrandom('' + seed); // this is inconsistent if you pass a number instead of a string
+  mySeededRng = new Math.seedrandom('' + seed); // this is inconsistent if you pass a number instead of a string
 
   let pokemonOnTheBoard = [];
 

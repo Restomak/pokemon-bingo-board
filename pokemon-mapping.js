8ccc66c7a8d9163fe0c3016e6c0176ca1d80649a
Restomak <38076002+Restomak@users.noreply.github.com>
2022-04-16 21:59:03 -0400

    weighting for loved/favourite pokemon

--- pokemon-mapping.js
+++ pokemon-mapping.js
@@ -34,7 +34,9 @@ function randomizeBoard(reroll = false) {
   let porygonFound = false;
   let cyndaquilFound = false;
   let umbreonFound = false;
-  let excludedPokemonFound = false;
+  let excludedPokemonFound = 0;
+  let favouritePokemonFound = 0;
+  let lovedPokemonFound = 0;
   let giveUpTimer = 0;
   let exitLoop = false;
   do {
@@ -69,9 +71,89 @@ function randomizeBoard(reroll = false) {
                 pokeNum <= 275 - 1 && pokeNum >= 273 - 1 || //seedot line
                 pokeNum <= 240 - 1 && pokeNum >= 236 - 1 || //magby, elekid, smoochum, hitmontop, tyrogue
                 pokeNum == 174 - 1 || //igglybuff
-                pokeNum == 173 - 1 || //cleffa
-                pokeNum == 354 - 1) {
-              excludedPokemonFound = true;
+                pokeNum == 173 - 1) { //cleffa
+              excludedPokemonFound++;
+            }
+            if (pokeNum == 001 - 1 || //bulbasaur
+                pokeNum == 004 - 1 || //charmander
+                pokeNum == 007 - 1 || //squirtle
+                pokeNum == 104 - 1 || //cubone
+                pokeNum == 132 - 1 || //ditto
+                pokeNum >= 134 - 1 && pokeNum <= 136 - 1 || //gen 1 eevee evos
+                pokeNum == 148 - 1 || //dragonair
+                pokeNum == 151 - 1 || //mew
+                pokeNum == 152 - 1 || //chikorita
+                pokeNum == 158 - 1 || //totodile
+                pokeNum == 177 - 1 || //natu
+                pokeNum == 194 - 1 || //wooper
+                pokeNum == 196 - 1 || //espeon
+                pokeNum == 206 - 1 || //dunsparse
+                pokeNum == 213 - 1 || //shuckle
+                pokeNum == 216 - 1 || //teddiursa
+                pokeNum == 225 - 1 || //delibird
+                pokeNum >= 235 - 1 || //smaergle
+                pokeNum >= 248 - 1 || //tyrannitar
+                pokeNum >= 292 - 1 || //shedinja
+                pokeNum == 363 - 1) { //spheal
+              favouritePokemonFound++;
+            }
+            if (pokeNum == 002 - 1 || //ivysaur
+                pokeNum == 003 - 1 || //venusaur
+                pokeNum == 006 - 1 || //charizard
+                pokeNum == 009 - 1 || //blastoise
+                pokeNum == 010 - 1 || //caterpie
+                pokeNum == 012 - 1 || //butterfree
+                pokeNum == 019 - 1 || //rattata
+                pokeNum == 025 - 1 || //pikachu
+                pokeNum == 027 - 1 || //sandshrew
+                pokeNum == 037 - 1 || //vulpix
+                pokeNum == 038 - 1 || //ninetails
+                pokeNum == 046 - 1 || //paras
+                pokeNum == 059 - 1 || //arcanine
+                pokeNum == 073 - 1 || //tentacruel
+                pokeNum == 077 - 1 || //ponyta
+                pokeNum == 086 - 1 || //seel
+                pokeNum == 087 - 1 || //dewgong
+                pokeNum >= 089 - 1 && pokeNum <= 091 - 1 || //muk, shellder, cloyster
+                pokeNum >= 094 - 1 || //gengar
+                pokeNum >= 099 - 1 || //kingler
+                pokeNum >= 111 - 1 || //rhyhorn
+                pokeNum >= 121 - 1 || //starmie
+                pokeNum >= 123 - 1 || //scyther
+                pokeNum >= 126 - 1 || //magmar
+                pokeNum >= 128 - 1 || //tauros
+                pokeNum >= 131 - 1 || //lapras
+                pokeNum >= 133 - 1 || //eevee
+                pokeNum == 142 - 1 || //aerodactyl
+                pokeNum == 144 - 1 || //articuno
+                pokeNum >= 145 - 1 || //zapdos
+                pokeNum == 147 - 1 || //dratini
+                pokeNum >= 153 - 1 || //bayleef
+                pokeNum >= 157 - 1 || //typhlosion
+                pokeNum >= 160 - 1 || //feraligatr
+                pokeNum >= 178 - 1 || //xatu
+                pokeNum == 179 - 1 || //mareep
+                pokeNum >= 182 - 1 || //bellossom
+                pokeNum >= 191 - 1 || //sunkern
+                pokeNum >= 211 - 1 || //quilfish
+                pokeNum >= 212 - 1 || //scizor
+                pokeNum >= 221 - 1 || //piloswine
+                pokeNum >= 227 - 1 && pokeNum <= 231 - 1 || //skarmory, houndour,houndoom, kingdra, phanpy
+                pokeNum >= 241 - 1 || //miltank
+                pokeNum >= 242 - 1 || //blissey
+                pokeNum >= 246 - 1 || //larvitar
+                pokeNum >= 255 - 1 || //torchic
+                pokeNum >= 258 - 1 || //mudkip
+                pokeNum >= 282 - 1 || //gardevoir
+                pokeNum >= 285 - 1 || //shroomish
+                pokeNum >= 300 - 1 || //skitty
+                pokeNum >= 304 - 1 || //aron
+                pokeNum >= 306 - 1 || //aggron
+                pokeNum >= 321 - 1 || //wailord
+                pokeNum >= 322 - 1 || //numel
+                pokeNum >= 325 - 1 || //spoink
+                pokeNum == 377 - 1) { //regirock
+              lovedPokemonFound++;
             }
             const chosenPoke = pokemonData[pokeNum]
             pokemonOnTheBoard.push(pokeNum);
@@ -99,9 +181,8 @@ function randomizeBoard(reroll = false) {
     if (gen == '1' ||
         !reroll && porygonFound ||
         porygonFound && cyndaquilFound && umbreonFound ||
-        porygonFound && (cyndaquilFound || umbreonFound) && giveUpTimer > 50 ||
-        porygonFound && !excludedPokemonFound && giveUpTimer > 100 ||
-        porygonFound && giveUpTimer > 150 ||
+        porygonFound && (cyndaquilFound || umbreonFound) && giveUpTimer > 50 - favouritePokemonFound * 4 - lovedPokemonFound * 2 + excludedPokemonFound * 3 ||
+        porygonFound && giveUpTimer > 100 - favouritePokemonFound * 4 - lovedPokemonFound * 2 + excludedPokemonFound * 3 ||
         giveUpTimer > 200) {
       exitLoop = true;
     } else {
